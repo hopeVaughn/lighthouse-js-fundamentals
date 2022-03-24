18fd611e55242811712bbccf405a6f7345f9ca44
Hope Warren <hopevaughnwarren@gmail.com>
2022-03-24 17:31:14 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -1,4 +1,4 @@
-const temperature = 21;
+/* const temperature = 21;
 
 
 if (temperature < 0) {
@@ -9,4 +9,22 @@ if (temperature < 0) {
   console.log("Short sleeves are fine.");
 }
 
-console.log("Now you're ready to go outside!");
+console.log("Now you're ready to go outside!"); */
+
+/* const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18){
+  console.log("You are eligible to vote.");
+} */
+
+// const temperature = 42;
+
+// if (temperature < -40 || temperature > 40){
+//   console.log("Maybe going outside isn't such a great idea...");
+// }
+
+const raining = true;
+if(!raining) {
+  console.log("Leave your umbrella at home!");
+}
